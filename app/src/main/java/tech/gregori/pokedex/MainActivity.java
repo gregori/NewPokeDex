c47263b47de976b993ca65677118e4e3ba4bcdd3
Rodrigo Gregori <rodrigo.gregori@gmail.com>
2021-06-17 22:11:15 -0300

    Adicionar funcionalidade para o recyclerview

--- app/src/main/java/tech/gregori/pokedex/MainActivity.java
+++ app/src/main/java/tech/gregori/pokedex/MainActivity.java
@@ -1,9 +1,13 @@
 package tech.gregori.pokedex;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -20,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pokemonList = new PokemonList(new ArrayList<>());
+
+        // obtém e prepara a recyclerView
+        RecyclerView recyclerView = findViewById(R.id.poke_recyclerview);
+        PokemonRecyclerAdapter recyclerAdapter = new PokemonRecyclerAdapter(getApplicationContext(),
+                pokemonList.getPokemon());
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(recyclerAdapter);
 
         PokemonService service = PokemonApiClient.getClient().create(PokemonService.class);
         Call<PokemonList> call = service.getPokemonList();
@@ -28,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<PokemonList> call, Response<PokemonList> response) {
                 pokemonList = response.body();
                 Log.d(TAG, "onResponse: " + pokemonList);
+                // atualiza lista de pokemons quando terminar o download
+                recyclerAdapter.setPokemons(pokemonList.getPokemon());
             }
 
             @Override
